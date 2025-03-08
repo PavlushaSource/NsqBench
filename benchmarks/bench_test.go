1cd5aff24a4adf1d608c714b61724e52cc79682a
PavlushaSource <pavlushkin.dima1785@gmail.com>
2025-03-08 14:57:38 +0300

    feat: Add few bench

--- benchmarks/bench_test.go
+++ benchmarks/bench_test.go
@@ -1,8 +1,9 @@
 package benchmarks
 
 import (
-	"fmt"
+	"github.com/PavlushaSource/NsqBench/src/services/domain"
 	"github.com/PavlushaSource/NsqBench/src/services/requestAdapters/serviceRequestNow"
+	"github.com/PavlushaSource/NsqBench/src/services/requestAdapters/serviceRequestOpt"
 	"github.com/PavlushaSource/NsqBench/src/services/responseAdapters/serviceResponseNow"
 	"sync"
 	"testing"
@@ -15,13 +16,13 @@ type testCase struct {
 
 var TestTable = []testCase{
 	{
-		name: "Sending 10^2 message",
-		iter: 3,
+		name: "Sending 10 message",
+		iter: 10,
 	},
-	//{
-	//	name: "Sending 10^5 message",
-	//	iter: 10e5,
-	//},
+	{
+		name: "Sending 10^5 message",
+		iter: 10000,
+	},
 	//{
 	//	name: "Sending 10^7 message",
 	//	iter: 10e7,
@@ -30,9 +31,40 @@ var TestTable = []testCase{
 
 func BenchmarkNSQMessageNow(b *testing.B) {
 	for _, tc := range TestTable {
-		fmt.Println("TEST " + tc.name)
 		b.Run(tc.name, func(b *testing.B) {
 			requester, err := serviceRequestNow.NewServiceRequest("127.0.0.1:4161", "127.0.0.1:4150")
+			if err != nil {
+				b.Error(err)
+			}
+			responser, err := serviceResponseNow.NewServiceResponse("127.0.0.1:4161", "127.0.0.1:4150", tc.iter)
+			if err != nil {
+				b.Error(err)
+			}
+
+			b.ResetTimer()
+
+			wg := sync.WaitGroup{}
+			wg.Add(1)
+			go func() {
+				if err := responser.Run(); err != nil {
+					b.Error(err)
+				}
+				defer responser.Close()
+				defer wg.Done()
+			}()
+
+			if err = requester.Run(tc.iter); err != nil {
+				b.Error(err)
+			}
+			wg.Wait()
+		})
+	}
+}
+
+func BenchmarkNSQMessageOpt(b *testing.B) {
+	requester, err := serviceRequestOpt.NewServiceRequestOpt("127.0.0.1:4161", "127.0.0.1:4150", domain.ResponseTopic, domain.ResponseChannel)
+	for _, tc := range TestTable {
+		b.Run(tc.name, func(b *testing.B) {
 			if err != nil {
 				b.Error(err)
 			}
